f049412d5e58f6488a6361dc80c1e7917ca77fd7
Takumi Mito <250158hj@yse-c.net>
2026-05-18 15:57:17 +0900

    レイアウトとテンプレート化

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,3 +1,28 @@
-<div>
-    <!-- We must ship. - Taylor Otwell -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practice PHP Laravel</title>
+</head>
+
+<body>
+    <header>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+    </header>
+
+    <!-- Contents -->
+    {{ $slot }}
+
+    <footer>
+        <ul>
+            <li><a href="/product/1">Coffee</a></li>
+            <li><a href="/product/2">Tea</a></li>
+            <li><a href="/product/3">Green Tea</a></li>
+        </ul>
+    </footer>
+</body>
+
+</html>
--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -1,6 +1,7 @@
-<h1>Product Page!</h1>
-
-<div>
-    <h2>商品名：{{ $product['name'] }}</h2>
-    <h3>価格：&yen;{{ $product['price'] }}</h3>
-</div>
+<x-layout>
+    <h1>Product Page!</h1>
+    <div>
+        <h2>商品名：{{ $product['name'] }}</h2>
+        <h3>価格：&yen;{{ $product['price'] }}</h3>
+    </div>
+</x-layout>
